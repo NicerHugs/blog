180d2f1136083b1235c633afd1143590ffea91ff
Jess Scheuring <jscheuring@gmail.com>
2016-10-17 22:58:47 -0500

    about page content

---
+++ about.js
@@ -0,0 +1,3 @@
+var height = document.querySelector('.about .about-wrapper img').height + 'px';
+document.querySelector('.mask').style.height = height;
+document.querySelector('.about-wrapper').style['min-height'] = height; 
