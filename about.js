a1c2cdfeac889846780422f24cdfd37f0e62971b
Jess Scheuring <jscheuring@gmail.com>
2016-10-20 22:10:26 -0500

    corrects load time on image

--- about.js
+++ about.js
@@ -1,3 +1,13 @@
-var height = document.querySelector('.about .about-wrapper img').height + 'px';
-document.querySelector('.mask').style.height = height;
-document.querySelector('.about-wrapper').style['min-height'] = height; 
+function resize(img) {
+	var height = img.height + 'px';
+	document.querySelector('.mask').style.height = height;
+	document.querySelector('.about-wrapper').style['min-height'] = height;
+}
+
+var img = document.querySelector('.about .about-wrapper img');
+
+if (img.complete) {
+	resize(img);
+} else {
+	img.addEventListener('load', resize.bind(null, img));
+}
